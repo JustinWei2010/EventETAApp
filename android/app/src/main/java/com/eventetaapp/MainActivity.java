a51e22833248f09e33e4c0482ebd40b71eb0b85c
Justin Wei <justinwei1991@gmail.com>
2017-01-05 01:04:55 -0800

    Setup fbsdk

--- android/app/src/main/java/com/eventetaapp/MainActivity.java
+++ android/app/src/main/java/com/eventetaapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.eventetaapp;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+
+//FBSDK override
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "EventETAApp";
+    }
+
+    //FBSDK override
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
